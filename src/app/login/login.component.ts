95b40ef1e68e84fbbb6dc7ce2d809468b83f4c41
OleksandrS <oleksandr.salivon@kanini.com>
2021-11-05 11:18:55 +0200

    from dashbord to app=modile

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,8 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
+export class LoginComponent implements OnInit {
+  hide = true;
   constructor() { }
 
   ngOnInit(): void {
