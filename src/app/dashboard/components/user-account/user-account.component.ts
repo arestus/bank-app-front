7617453cb65311be4a95c66cac0766ca99dd7f88
unknown <Administrator@KANINI-LTP-470.gisbizdev.gisbiz.chennai.com>
2021-11-10 01:32:33 +0530

    user-account and user-profile page added

--- src/app/dashboard/components/user-account/user-account.component.ts
+++ src/app/dashboard/components/user-account/user-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-account',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAccountComponent implements OnInit {
 
-  constructor() { }
+  currentBalance = 4654;
+  savingBalance =5245; 
+  constructor(private _router:Router) { }
+  ShowBalance(){
+    {
+      if (localStorage.getItem('induk-bank-user')) {
+        let storage = localStorage.getItem('induk-bank-user');
+        let parsedStorage = JSON.parse(storage as string);
+        this.currentBalance = parsedStorage.currentBalance;             
+        this.savingBalance= parsedStorage.savingBalance;   
+        console.log(parsedStorage);
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
 
   ngOnInit(): void {
+  
+ 
   }
 
 }
--- src/app/dashboard/components/user-profile/user-profile.component.ts
+++ src/app/dashboard/components/user-profile/user-profile.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
+  customerID: number = 0;
+  email:string ='';
+  savingAccount:number =0;
+  currentAccount:number =0;
+  savingBalance:number =0;
+  currentBalance:number =0;
+  name:string ='';
+  password:string ='';
+  phone:string ='';
+  address:string ='';
+  paNnumber:string ='';
+  aadhaarnumber:string ='';
+  dateOfBirth:string ='';
+  //jwtToken:string ='';
+  //role:string ='';
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  ShowUserProfile() {
+    if (localStorage.getItem('induk-bank-user')) {
+      let storage = localStorage.getItem('induk-bank-user');
+      let parsedStorage = JSON.parse(storage as string);
+      this.customerID = parsedStorage.customerID; 
+      this.name = parsedStorage.name;
+      this.dateOfBirth=parsedStorage.dateOfBirth;    
+      this.email =parsedStorage.email;
+      this.password = parsedStorage.password;     
+      this.phone = parsedStorage.phone;
+      this.address = parsedStorage.address;
+      this.paNnumber = parsedStorage.paNnumber;
+      this.aadhaarnumber = parsedStorage.aadhaarnumber;      
+      console.log(parsedStorage);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 
 }
