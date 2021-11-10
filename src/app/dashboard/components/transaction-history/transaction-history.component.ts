12bdac00d96fafee32b2191d9c286f74c4ba786b
OleksandrS <oleksandr.salivon@kanini.com>
2021-11-10 13:47:12 +0200

    pdf print Added

--- src/app/dashboard/components/transaction-history/transaction-history.component.ts
+++ src/app/dashboard/components/transaction-history/transaction-history.component.ts
@@ -1,26 +1,39 @@
-
 import { Router } from '@angular/router';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  VERSION,
+  ViewChild,
+} from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 
 import { TableService } from 'src/app/services/table.service';
 
+import jsPDF from 'jspdf';
+
 @Component({
   selector: 'app-transaction-history',
   templateUrl: './transaction-history.component.html',
-  styleUrls: ['./transaction-history.component.css']
+  styleUrls: ['./transaction-history.component.css'],
 })
 export class TransactionHistoryComponent implements OnInit {
   dataSource = new MatTableDataSource<any>();
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild('htmlData') htmlData!: ElementRef;
 
   form = new FormGroup({
     fromDate: new FormControl(null, { validators: [Validators.required] }),
-    toDate: new FormControl(null, { validators: [Validators.required] })
+    toDate: new FormControl(null, { validators: [Validators.required] }),
   });
 
   displayedColumns: string[] = [
@@ -30,19 +43,60 @@ export class TransactionHistoryComponent implements OnInit {
     'fromAccount',
     'toAccount',
     'amount',
-    'transactionStatus'];
-  constructor(private router: Router,public tableService: TableService) {
-    
-   }
+    'transactionStatus',
+  ];
+  constructor(private router: Router, public tableService: TableService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   applyDateFilter() {
-    this.tableService.dataSource = this.tableService.dataSource.filter(e => 
-      e.transactionDate >= this.form.value.fromDate.toISOString().replace(/.\d+Z$/g, "") && e.transactionDate <= this.form.value.toDate.toISOString().replace(/.\d+Z$/g, "")
+    this.tableService.dataSource = this.tableService.dataSource.filter(
+      (e) =>
+        e.transactionDate >=
+          this.form.value.fromDate.toISOString().replace(/.\d+Z$/g, '') &&
+        e.transactionDate <=
+          this.form.value.toDate.toISOString().replace(/.\d+Z$/g, '')
     );
     console.log(this.tableService.dataSource, 'this.tableService.dataSource');
   }
 
+  // PDF
+  name: string = 'Angular ' + VERSION.major;
+
+  public openPDF(): void {
+    const Data = this.htmlData.nativeElement;
+    const doc = new jsPDF('p', 'px', 'a4');
+    const doc2 = new jsPDF();
+    doc.html(Data, {
+      autoPaging: true,
+      width: 20,
+      x: 15,
+      y: 10,
+      windowWidth: 20,
+      callback: function (doc) {
+        doc.output('dataurlnewwindow');
+      },
+    });
+  }
+
+  public downloadPDF(): void {
+    const data = this.htmlData.nativeElement;
+    const doc = new jsPDF('p', 'px', 'a4');
+    const handleElement = {
+      '#editor'(element: any, renderer: any): boolean {
+        return true;
+      },
+    };
+
+    doc.html(data, {
+      autoPaging: true,
+      width: 20,
+      x: 15,
+      y: 10,
+      windowWidth: 20,
+      callback: function (doc) {
+        doc.save('angular-demo.pdf');
+      },
+    });
+  }
 }
